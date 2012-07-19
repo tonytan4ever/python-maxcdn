da555de0c6547aaefb8e668a1de5404d804a3144
David Litvak Bruno <david.litvakb@gmail.com>
2012-07-19 17:39:13 -0300

    debug everywhere

--- netdnarws/netdna.py
+++ netdnarws/netdna.py
@@ -38,32 +38,58 @@ class NetDNA(object):
                                   uri
                                 )
 
-    def _response_as_json(self, method, uri, **kwargs):
+    def _get_content_length(self, data):
+        if data:
+            return len(requests.models.Request._encode_params(data))
+        return 0
+
+    def _get_content_length_header(self, data):
+        return {'Content-Length': str(self._get_content_length(data))}
+
+    def _response_as_json(self, method, uri, *args, **kwargs):
+        headers = None
+        debug_json = kwargs.pop('debug_json', False)
+        debug_request = kwargs.pop('debug_request', False)
+
         if kwargs.pop('debug', False):
-            print "Making %s request to %s\n" % (method.upper(), self._get_url(uri))
-        response = getattr(self.client, method)(self._get_url(uri), **kwargs)
-        try:
-            if response.status_code != 200:
-                raise Exception("%d: %s" % (
-                                 response.status_code,
-                                 response.json['error']['message'])
-                               )
-        except TypeError:
-            raise Exception(
-              "%d: No Error information supplied by the server" % (
-              response.status_code,)
-            )
+            print "Making %s request to %s\n" % (method.upper(),
+                                                 self._get_url(uri))
+        if kwargs.pop('override_headers', False):
+             headers = self._get_content_length_header(
+               kwargs.get('data', None))
+
+        response = getattr(self.client, method)(
+                     self._get_url(uri),
+                     headers = headers
+                     *args, **kwargs
+                   )
+
+        if debug_request:
+            return response
+
+        if not debug_json:
+            try:
+                if response.status_code != 200:
+                    raise Exception("%d: %s" % (
+                                     response.status_code,
+                                     response.json['error']['message'])
+                                   )
+            except TypeError:
+                raise Exception(
+                  "%d: No Error information supplied by the server" % (
+                  response.status_code,)
+                )
 
         return response.json
 
     def get(self, uri, **kwargs):
         return self._response_as_json("get", uri, **kwargs)
 
-    def post(self, uri, data={}, **kwargs):
+    def post(self, uri, data=dict(), **kwargs):
         return self._response_as_json("post", uri, data=data, **kwargs)
 
-    def put(self, uri, **kwargs):
-        return self._response_as_json("put", uri, **kwargs)
+    def put(self, uri, data=dict(), **kwargs):
+        return self._response_as_json("put", uri, data=data, **kwargs)
 
     def delete(self, uri, **kwargs):
         return self._response_as_json("delete", uri, **kwargs)
