54c77cc714ec0cf09f97209968898a632d745f98
Josh Mervine <jmervine@yp.com>
2014-02-08 14:12:11 -0800

    Hoping to fix 3.x travis runs.

--- Makefile
+++ Makefile
@@ -17,14 +17,14 @@ cov_opts= --with-coverage --cover-package=maxcdn
 ###
 init: clean setup test
 
-setup:
+setup: distribute
 	pip install -r requirements.txt -t $(target) -b $(source)
 
 clean:
 	rm -rf $(source) $(target) .ropeproject .coverage junit-report.xml
 	find . -type f -name "*.pyc" -exec rm -v {} \;
 
-coverage:
+coverage: build/coverage
 	$(pypath) python $(nose) $(cov_opts) $(tests)
 
 test:
@@ -44,6 +44,12 @@ test/help:
 	#$(pypath) python3.3 $(nose) $(test_opts) $(tests)
 
 travis: setup test
+
+distribute:
+	pip install distribute
+
+build/coverage:
+	pip install coverage -t $(target) -b $(source)
 
 .PHONY: init clean test coverage test/help test/32 test/33
 
--- setup.py
+++ setup.py
@@ -1,21 +1,41 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from setuptools import setup
 
-setup(
-    name="maxcdn",
-    version="0.0.1",
-    description="A Python REST Client for MaxCDN REST Web Services",
-    author="Joshua P. Mervine",
-    author_email = "joshua@mervine.net",
-    license = "MIT",
-    keywords = "MAxCDN CDN API REST",
-    packages=['maxcdn'],
-    include_package_data=True,
-    install_requires=[
-      'requests',
-      'requests_oauthlib',
-      'certifi'
-    ],
-    url='http://www.maxcdn.com',
-)
+options = {
+    "name"                  : "maxcdn",
+    "version"               : "0.0.1",
+    "description"           : "A Python REST Client for MaxCDN REST Web Services",
+    "author"                : "Joshua P. Mervine",
+    "author_email"          : "joshua@mervine.net",
+    "license"               : "MIT",
+    "keywords"              : "MAxCDN CDN API REST",
+    "packages"              : ['maxcdn'],
+    "url"                   : 'http://www.maxcdn.com'
+}
+
+# additional setuptools data (python 2.x)
+install_requires = [
+  "requests",
+  "requests_oauthlib",
+  "certifi",
+  "nose",
+  "mock"
+]
+include_package_data = True
+
+try:
+    from setuptools import setup
+    options["install_requires"] = install_requires
+    options["include_package_data"] = include_package_data
+    setup(**options)
+
+except ImportError:
+    print("ERROR: setuptools wasn't found, please install it")
+
+    #from distutils.core import setup
+    #setup(**options)
+    #print("WARNING: setuptools wasn't found, either install")
+    #print(" setuptools and rerun setup.py or manually install:")
+    #for p in install_requires:
+        #print("   - "+p)
+
